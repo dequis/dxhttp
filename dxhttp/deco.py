f701f619ca22bb8d6c2ac93632ec8d5de9b288e8
dequis <dx@dxzone.com.ar>
2009-08-15 16:22:45 -0300

    Add *args and **kwargs to decorators

--- dxhttp/deco.py
+++ dxhttp/deco.py
@@ -5,9 +5,9 @@ import functools
 def content_type(type):
     def decorator(f):
         @functools.wraps(f)
-        def wrapper(environ, start_response):
+        def wrapper(environ, start_response, *args, **kwargs):
             start_response('200 OK', [('content-type', type)])
-            return f(environ, start_response)
+            return f(environ, start_response, *args, **kwargs)
         return wrapper
     return decorator
 
@@ -16,8 +16,8 @@ html = content_type(config.CONTENT_HTML)
 def args(f):
     '''Expands dxhttp.args into actual function arguments'''
     @functools.wraps(f)
-    def wrapper(environ, start_response):
-        return f(environ, start_response, *environ['dxhttp.args'])
+    def wrapper(environ, start_response, *args, **kwargs):
+        return f(environ, start_response, *(environ['dxhttp.args'] + args), **kwargs)
     return wrapper
 
 
