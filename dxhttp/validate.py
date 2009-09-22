8930a3571d3792bf1a657a064b18831d5e2f8bda
dequis <dx@dxzone.com.ar>
2009-09-22 17:05:59 -0300

    validate: docs, cleanup, GET support
    The validation code is moved to _validate(), while the decorators are
    split in @deco (base one), @post and @get.
    New parameter "doraise" which defaults to False, if enabled the
    exception is raised, otherwise it is returned to output.

--- dxhttp/validate.py
+++ dxhttp/validate.py
@@ -1,36 +1,85 @@
+'''Higher-level form parser that ensures that required fields are there
+and converts specified fields to ints. The parsed form is passed to the
+function as a third parameter, "form".
 
+If there are errors, the original function is not called and an error
+message is returned instead (if doraise=False) or FormValidationException
+is raised (if doraise=True, duh)
+
+File uploads not supported yet, but are easy to implement.
+
+Usage:
+    
+    @deco.html
+    @validate.post(fields=['name', 'address', 'age', 'comments'],
+                   ints=['age'],
+                   notnull=['name', 'address', 'age'])
+    def submit_something(environ, start_response, form):
+        if form['age'] < 13:
+            return ['this is illegal']
+        return ['right']
+'''
+
+import cgi
 import functools
-from dxhttp.post import *
-
+import dxhttp.post
 
 class FormValidationException(Exception):
     pass
 
+def _validate(environ, method, fields, ints, notnull):
+    '''Validates a form a returns a dict with the values
+    
+    -method: POST or GET
+    -fields: keys to include in the dict. Missing fields default to ''
+    -ints: fields that are converted to int
+    -notnull: required fields
 
-def post(fields=[], ints=[], notnull=[]):
+    Not meant to be used directly, try the "post" and "get" decorators instead
+    '''
+    
+    if method == 'POST':
+        if not dxhttp.post.is_post_request(environ):
+            raise FormValidationException('POST only')
+        form = dxhttp.post.get_post_form(environ)
+    else:
+        form = cgi.FieldStorage(environ=environ, keep_blank_values=1)
+
+    newform = {}
+    for field in fields:
+        try:
+            newform[field] = form[field].value
+        except KeyError:
+            newform[field] = ''
+
+        if field in notnull and \
+           newform[field] is None or len(str(newform[field])) == 0:
+            raise FormValidationException('Field %s required' % field)
+
+        if field in ints:
+            try:
+                newform[field] = int(newform[field])
+            except ValueError:
+                raise FormValidationException('Field %s must be int' % field)
+    return newform
+
+def deco(method, fields=[], ints=[], notnull=[], doraise=False):
     def decorator(f):
         @functools.wraps(f)
         def wrapper(environ, start_response, *args, **kwargs):
-            if not is_post_request(environ):
-                raise FormValidationException('POST only')
-            
-            form = get_post_form(environ)
-            newform = {}
-            for field in fields:
-                try:
-                    newform[field] = form[field].value
-                except KeyError:
-                    newform[field ] = ''
-
-                if newform[field] is None or len(str(newform[field])) == 0:
-                    raise FormValidationException('Field %s required' % field)
-
-                if field in ints:
-                    try:
-                        newform[field] = int(newform[field])
-                    except ValueError:
-                        raise FormValidationException('Field %s must be int' % field)
-
+            try:
+                newform = _validate(environ, method, fields, ints, notnull)
+            except FormValidationException, e:
+                if doraise:
+                    raise
+                else:
+                    return ['Form validation error: %s' % e]
             return f(environ, start_response, newform, *args, **kwargs)
         return wrapper
     return decorator
+
+def post(fields=[], ints=[], notnull=[], doraise=False):
+    return deco('POST', fields, ints, notnull, doraise)
+
+def get(fields=[], ints=[], notnull=[], doraise=False):
+    return deco('GET', fields, ints, notnull, doraise)
