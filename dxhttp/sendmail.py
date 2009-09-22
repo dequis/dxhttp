4835d6cebb1f0796a8d879c36ddba292ebb6fc92
dequis <dx@dxzone.com.ar>
2009-09-22 18:40:54 -0300

    sendmail: error checking, added is_valid_email()

--- dxhttp/sendmail.py
+++ dxhttp/sendmail.py
@@ -1,6 +1,7 @@
 
 import config
 
+import re
 import subprocess
 
 def mail(to="", subject="", text="", html=True, **headers):
@@ -11,21 +12,34 @@ def mail(to="", subject="", text="", html=True, **headers):
     Underscores are replaced with dashes in headers:
         mail(..., Content_Type="text/html")
     Newlines are stripped from header values.
+    From and To fields are validated. Returns False on any error.
     '''
-    process = subprocess.Popen([config.SENDMAIL_PATH, '-t', to],
-        stdin=subprocess.PIPE)
+    
+    headers.setdefault('Subject', subject)
+    headers.setdefault('From', config.DEFAULT_FROM)
+    headers['To'] = to
+    
+    if not is_valid_email(to) or not is_valid_email(headers['From']):
+        return False
+    
+    if html:
+        headers.setdefault('Content_Type', "text/html")
 
-    if 'Subject' not in headers:
-        headers['Subject'] = subject
-
-    if 'From' not in headers and config.DEFAULT_FROM:
-        headers['From'] = config.DEFAULT_FROM
-
-    if html and 'Content_Type' not in headers:
-        headers['Content_Type'] = "text/html"
+    try:
+        process = subprocess.Popen([config.SENDMAIL_PATH, '-t', to],
+            stdin=subprocess.PIPE)
+    except OSError:
+        return False
 
     for key, value in headers.iteritems():
         process.stdin.write("%s: %s\n" % (key.replace("_", "-"),
                                           value.replace("\n", "")))
     process.stdin.write("\n")
     process.stdin.write(text)
+    return process.wait() == 0
+
+valid_re = re.compile('^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$', re.I)
+def is_valid_email(email):
+    '''Regexp stolen from "somewhere"'''
+    return valid_re.match(email) is not None
+
