ccf203c3e2394e22d5a75bd7785c206ee4dcd187
dequis <dx@dxzone.com.ar>
2009-10-12 12:46:05 -0300

    exc: Added MailExceptionMiddleware

--- dxhttp/exc.py
+++ dxhttp/exc.py
@@ -5,7 +5,7 @@ import traceback
 import functools
 import cStringIO as StringIO
 
-from dxhttp import utils
+from dxhttp import sendmail
 import config
 
 STATUS = '500 INTERNAL SERVER ERROR'
@@ -27,6 +27,35 @@ def RogerExceptionMiddleware(app):
 
             yield exception
     return wrapper
+
+
+def MailExceptionMiddleware(mail, error_message):
+    '''Sends the traceback to (mail)
+    and displays (error_message) to the user'''
+
+    def decorator(app):
+        @functools.wraps(app)
+        def wrapper(environ, start_response):
+            try:
+                appiter = app(environ, start_response)
+                for item in appiter:
+                    yield item
+            except:
+                exception = traceback.format_exc()
+                try:
+                    start_response(STATUS, HEADERS)
+                except:
+                    pass
+                yield error_message
+
+                subject = "%s: %s" % (environ["SERVER_NAME"], exception.split("\n")[-1])
+                dump =  '\n'.join([': '.join(map(str, x)) for x in environ.items()])
+                text = '%s\n\nEnviron dump:\n%s' % (exception, dump)
+                sendmail.mail(to=mail, subject=subject, text=text, html=False)
+
+        return wrapper
+    return decorator
+
 
 if config.DEBUG:
     try:
