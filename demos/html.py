df56b154fe67e1fc56ed4d7a3b13528043f88d63
dequis <dx@dxzone.com.ar>
2009-12-21 09:45:44 -0300

    Removed demos

--- demos/html.py
+++
@@ -1,8 +0,0 @@
-import html
-
-def main(environ, node):
-    lol = node.tag('div', id="lol")
-    lol.tag("h1", "lol")
-    lol.tag("p", "ololol: ").tag("b", "LOL")
-    node.tag("hr")
-    node.tag("h2", "lol'd").tag("sup", "tm")
