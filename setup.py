4aa5e1d9b0ff7ee79e89db90664211637875393a
dequis <dx@dxzone.com.ar>
2010-01-28 18:52:15 -0300

    Added dxhttp_project script to manage projects

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup, find_packages
 setup(
     name = "dxhttp",
-    version = "2.1",
+    version = "2.2",
     packages = find_packages(),
+    package_data = {
+        '': ['.htaccess', 'dxhttp.fcgi', 'main.py', 'config.py', 'app.py']
+    },
+    entry_points = {
+        'console_scripts': [
+            'dxhttp_project = util.project:main',
+        ]
+    }
 )
 
