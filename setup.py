d65dc870aa1e11b9b442c5ad3820b6b30bfb5e2e
dequis <dx@dxzone.com.ar>
2010-12-08 15:31:39 -0300

    Removed 'util' package

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 setup(
     name = "dxhttp",
     version = "2.3",
-    packages = find_packages(),
+    packages = ['dxhttp'],
     package_data = {
         '': ['.htaccess', 'dxhttp.fcgi', 'config.py', 'app.py']
     },
     entry_points = {
         'console_scripts': [
-            'dxhttp_project = util.project:main',
+            'dxhttp_project = dxhttp.project:main',
         ]
     }
 )
